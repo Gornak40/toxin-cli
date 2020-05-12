e9942d5e1758d57b6dad55173eb800b1186d4c73
Alexander Gorbunov <s-kozelsk@yandex.ru>
2020-05-12 09:02:03 +0300

    Rating commit
    Added IMDB and Kinopoisk rating showing

--- toxic.py
+++ toxic.py
@@ -32,11 +32,13 @@ enc = lambda x: str(x.encode('cp1251'))[2:-1].replace('\\x', '%').upper()
 date = lambda x: x[x.find(',') - 4:x.find(',')]
 dur = lambda x: search(r'\d\d\:\d\d', x).group(0) if search(r'\d\d\:\d\d', x) else None
 good = lambda x: [i for i in filter(bool, x)]
+rate = lambda x: search(r'/(\d+)\.', x).group(0)[1:-1]
 
 
 main = 'http://filmitorrent.net'
 home = '''http://filmitorrent.net/index.php?do=search&subaction=search&search_start=0&full_search=0&result_from=1&result_from=1&story='''
 name = ' '.join(argv[1:])
+# name = 'Джон уик' #
 url = home + enc(name)
 bs = urlbs(url)
 films = bs.select('#dle-content > div.post > div.post-title > a')
@@ -46,9 +48,18 @@ films = bs.select('#dle-content > div.post > div.data > span:nth-child(1)')
 dates = [date(x.text.strip()) for x in films]
 films = bs.select('#dle-content > div.post > div.post-story')
 durat = good([dur(x.text.strip()) for x in films])
-head = ['Название', 'Длительность', 'Год']
-ind = table(head, names, durat, dates)
+css = '#dle-content > div.post > div.data > div.cell2 > img'
+rates = [i.get('src') for i in bs.select(css)]
+rates = ['https://rating.kinopoisk.ru/{}.xml'.format(rate(i)) for i in rates]
+films = [urlbs(x) for x in rates]
+rkino = [round(float(x.select('rating > kp_rating')[0].text), 1) for x in films]
+rimdb = [round(float(x.select('rating > imdb_rating')[0].text), 1) for x in films]
+
+head = ['Название', 'Длительность', 'Год', 'Кинопоиск', 'IMDB']
+ind = table(head, names, durat, dates, rkino, rimdb)
 url = links[ind]
+
+
 bs = urlbs(url)
 css = '#dle-content > div.post > div.post-story > table'
 films = bs.select(css)[0]
